29f7b9d7bccc04e9f75b887d0c9f70222fa4b2f1
LuchoProgramer <luchoviteri1990@gmail.com>
2025-11-24 00:45:13 -0500

    Mejorar sitemap.ts para incluir dinámicamente todos los posts del blog - Auditoría SEO

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,9 +1,20 @@
 import { MetadataRoute } from 'next'
+import { HybridCMSService } from '@/lib/cms'
  
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://pukadigital.com'
   
-  return [
+  // Get all blog posts dynamically
+  const { posts } = await HybridCMSService.getAllPosts();
+  
+  const blogPostUrls: MetadataRoute.Sitemap = posts.map(post => ({
+    url: `${baseUrl}/blog/${post.slug}`,
+    lastModified: new Date(post.date),
+    changeFrequency: 'monthly',
+    priority: 0.7,
+  }));
+
+  const staticUrls: MetadataRoute.Sitemap = [
     {
       url: baseUrl,
       lastModified: new Date(),
@@ -35,4 +46,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.7,
     },
   ]
+  
+  return [...staticUrls, ...blogPostUrls]
 }
