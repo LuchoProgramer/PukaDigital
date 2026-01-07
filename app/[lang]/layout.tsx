77c80d19ccab9d3ec90c3a1736f940bd60cc0b2f
LuchoProgramer <luchoviteri1990@gmail.com>
2026-01-07 07:47:52 -0500

    feat: integrate Microsoft Clarity for user behavior tracking

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -193,6 +193,17 @@ export default async function LangLayout({
           `}
         </Script>
 
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "uxq6p6x3tm");
+          `}
+        </Script>
+
         {/* Global Schema: Organization + LocalBusiness + WebSite */}
         <script
           type="application/ld+json"
